25e238a4ee6622d0c7add8f96ef59ba2c985eb7d
Mateus de Barros <97922225+MateusDBarros@users.noreply.github.com>
2024-11-16 03:28:34 -0300

    Update Banco.java

--- ATM Uograde/src/Banco.java
+++ ATM Uograde/src/Banco.java
@@ -1,29 +1,44 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Random;
 
 public class Banco {
 
 
-    String name;
-    String accNum;
-    String password;
+    static String name;
+    static String accNum;
+    static String password;
+    static double cash = 0;
+
+    Banco (String name, String accNum, String password, double cash){
+        this.name = name;
+        this.accNum = accNum;
+        this.password = password;
+        this.cash = cash;
+    }
+
+    public static void showAll() {
+        System.out.printf("Titular: %s, Número: %s, Saldo: R$ %.2f%n", name, accNum, cash);
+    }
+
     public static int Accounts = 0;
 
     static Scanner scanner = new Scanner(System.in);
     static Random random = new Random();
+    ArrayList<Banco> contas = new ArrayList<Banco>();
 
     static void create(Banco contas[]) {
-        contas[Accounts] = new Banco();
-        System.out.printf("Digite o nome da contas: ");
-        contas[Accounts].name = scanner.nextLine();
-        System.out.printf("Digite a senha da sua conta");
-        contas[Accounts].password = scanner.nextLine();
-        int AccInt = random.nextInt(0,10001);
-        String AccString  = Integer.toString(AccInt);
-        contas[Accounts].accNum = AccString;
-        Accounts++;
 
-        System.out.println("Conta criada com sucesso!\nO numero da sua conta é: " + contas[Accounts].accNum);
+        System.out.printf("Digite o nome da contas: ");
+        String name = scanner.nextLine();
+        System.out.printf("Digite a senha da sua conta: ");
+        String password = scanner.nextLine();
+        int AccInt = random.nextInt(0,10001);
+        String accNum = Integer.toString(AccInt);
+        System.out.println("Conta criada com sucesso!");
+        contas[Accounts] = new Banco(name, accNum, password, 0);
+        showAll();
+        Accounts++;
     }
 
     static void cashOut(Banco contas[], int index) {
